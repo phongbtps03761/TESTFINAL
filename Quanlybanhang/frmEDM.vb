710fe0b639626fcceed106540a265642e00bad1f
phongbtps03761 <phongbtps03761@fpt.edu.vn>
2016-07-07 00:00:00 +0700

    Revert "234234"
    This reverts commit 40bf2b09da5849f1bd9f6b66c14182bf3a5a3360.

--- Quanlybanhang/frmEDM.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class frmEDM
-    Private Sub frmMinhVuongM4U_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        WebBrowser1.Navigate("https://www.youtube.com/embed/videoseries?list=PLRBp0Fe2GpgmsW46rJyudVFlY6IYjFBIK")
-    End Sub
-
-End Class
--- Quanlybanhang/frmGameCaro.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class frmGameCaro
-    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
-    End Sub
-
-    Private Sub frmGameCaro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        WebBrowser1.Navigate("http://gameflash.vn/games/co-caro.swf")
-    End Sub
-End Class
